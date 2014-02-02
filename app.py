943cfbb9df100e191cf33a105c6aefbc2fa1a836
Rushi Agrawal <rushi.agr@gmail.com>
2014-02-02 22:27:03 +0530

    Use Flask's 'flash' to display status messages on the website

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 from flask import request
 from flask import render_template
+from flask import flash
 
 #from db import api as db_api
 
@@ -10,6 +11,8 @@ app = Flask(__name__)
 
 # TODO: put this sensitive information into a new configuration file
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:nova@localhost/rushi'
+app.secret_key = 'Aenergee kayn needher beey creyaitaid nohr beiy deistroid'
+
 
 db = SQLAlchemy(app)
 
@@ -22,15 +25,12 @@ def main():
     if request.method =='POST':
         engine.store_transactions(request.form.get('inputText'),
                                       request.form.get('amc'), 1)
-        return 'OK. Saved in DB.'
-#        return 'OK, data submitted to server. Data:</br>' + request.form.get('inputText')
+        flash('OK. Saved in DB.')
+        mf_dict, stats = engine.get_summary(1)
+        return render_template('index.html', mf_dict=mf_dict, stats=stats)
     elif request.method == 'GET':
         mf_dict, stats = engine.get_summary(1)
         return render_template('index.html', mf_dict=mf_dict, stats=stats)
-#        txns = engine.get_txns()
-#        return ui.tablify_transactions(txns)
-#         f = open('webui/welcome_page.html')
-#         return ''.join(f.readlines())
 
 
 if __name__ == "__main__":
